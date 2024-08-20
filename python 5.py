a888812dc10745eba541d50b21988e6592180efb
aabidh <mmaabith99@gmail.com>
2024-08-20 20:56:08 +0530

    add python5

---
+++ python 5.py
@@ -0,0 +1,9 @@
+age = int(input("what is your age : "))
+if age >=50 :
+    print("your older")
+else :
+    print("your good to go")
+    3
+print(type(age))
+
+print("aabidh\'s")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+age = int(input("what is your age : "))
+if age >=50 :
+    print("your older")
+else :
+    print("your good to go")
+    
+print(type(age))
