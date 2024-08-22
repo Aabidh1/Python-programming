e64f57de89d101e0829c64649e77a8eb78373104
aabidh <mmaabith99@gmail.com>
2024-08-23 00:26:22 +0530

    operation

---
+++ python 6.py
@@ -0,0 +1,12 @@
+print(5+2)
+print(5**2)
+print(5-2)
+print(5/2)
+print(5//2) # floor division rounding value
+print(5%2) # modulo
+print(5*2)
+#excersie calculate BMI
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+bmi = weight/(height**2)
+print("Your BMI is: ", bmi)
