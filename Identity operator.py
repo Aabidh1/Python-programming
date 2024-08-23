b83c07f5247f36535d3f58eac26d448b1bfde936
aabidh <mmaabith99@gmail.com>
2024-08-24 00:32:52 +0530

    membership

--- Identity operator.py
+++ Identity operator.py
@@ -1,7 +1,7 @@
 #is 
 #is not
 a= 5
-b =5 # comopare the object memory location of index 1000da nadda random index comes
+b =5 # comopare the object memory location of index(ID looks) 1000da nadda random index comes
 #a and b  obejct of id(a) and id(b) a==b diffrent from a is b
 print(a is b )
 print(a is not b )
---
+++ Membership operator.py
@@ -0,0 +1,7 @@
+#in and not in check weather caharacter,substring value etc.. is present
+s = "harry potter"
+print('h' in s)
+print('h' not in s)
+l =[1,2,3,4,5,6,7,8,9]
+print(1 in l)
+print("rr" in s)
