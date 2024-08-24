6465da806994366f570881f67da497d38b66abbe
aabidh <mmaabith99@gmail.com>
2024-08-24 16:41:39 +0530

    cxv

--- leaf year exe7.py
+++ leaf year exe7.py
@@ -3,7 +3,5 @@ if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
             print("It is a leap year")
-    else :
-            print("It is not a leap year")
 else:
     print("It is not a leap year")
