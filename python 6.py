dab0485edda909b15781c171f9966a4b8ff0e2e6
aabidh <mmaabith99@gmail.com>
2024-08-24 12:38:05 +0530

    round

--- python 6.py
+++ python 6.py
@@ -5,8 +5,9 @@ print(5/2)
 print(5//2) # floor division rounding value
 print(5%2) # modulo
 print(5*2)
+print(round(2.675, 2)) # round to 2 decimal places
 #excersie calculate BMI
 weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in meters: "))
 bmi = weight/(height**2)
-print("Your BMI is: ", bmi)
+print("Your BMI is: ", round(bmi))
