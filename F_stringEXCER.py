905c64396971fd92e1dba148a4350a0125c3d288
aabidh <mmaabith99@gmail.com>
2024-08-24 14:11:28 +0530

    F_stringex

---
+++ F_stringEXCER.py
@@ -0,0 +1,6 @@
+age = int(input("What is your current age? "))
+liv = 90
+days = (liv - age) * 365
+weeks = (liv - age) * 52
+months = (liv - age) * 12
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
